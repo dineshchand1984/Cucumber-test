d1861ac4aa5146610a1c6a50ead4b2ab9b0a2fc2
dineshchand1984 <42776668+dineshchand1984@users.noreply.github.com>
2020-04-16 00:49:15 +0530

    Corrected cucumber output format options

--- src/test/java/com/example/bit/wonders/demo/CucumberTestRunner.java
+++ src/test/java/com/example/bit/wonders/demo/CucumberTestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin = {"json:src/target/cucumber-report.json"}, features = "src/test/resources/features")
-public class CucumberTestRunner {
-
-}
+@CucumberOptions(
+		          plugin   = {"pretty" ,"json:src/target/cucumber-report.json",
+		        		                "html:src/target/report.html"},
+                  features =  "src/test/resources/features",
+                  strict   =  false)
+public class CucumberTestRunner {}
